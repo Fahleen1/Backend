1fd63eb3de2e54f44e42361af861c0098dc2555b
Fahleen1 <mishaarif14@gmail.com>
2025-01-20 10:49:40 +0500

    set up environment variables, database connection, and add necessary dependencies

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -1,1 +1,14 @@
+import { DB_NAME } from '../constants.js';
 import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`,
+    );
+    console.log(`\n Mongodb connected..`);
+  } catch (error) {
+    console.log('Mongodb connection error', error);
+    process.exit(1);
+  }
+};
